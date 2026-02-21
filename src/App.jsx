cda464651bbb86784641edc5603e18823ef22723
Javier de Miguel <javierdemiguel@MacBook-Pro-de-Javier.local>
2026-02-21 07:44:46 +0100

    Actualización de testimonios y correcciones

--- src/App.jsx
+++ src/App.jsx
@@ -215,10 +215,10 @@ const TESTIMONIALS = [
   },
   { 
     id: 8, 
-    name: "Fabiola Jurado", 
+    name: "Roberto Moreno Durán", 
     initials: "FJ", 
     color: "bg-cyan-100 text-cyan-600", 
-    role: "HR Manager en MERCADONA", 
+    role: "Digital Project Manager UX & Procesos", 
     text: "La digitalización de nuestros procesos internos fue mucho más fluida gracias a su diseño centrado en el empleado y su facilidad de uso." 
   },
   { 
